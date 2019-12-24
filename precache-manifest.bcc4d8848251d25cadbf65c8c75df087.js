a1103abf86c9e9eabed33fa2bc74848f85af7748
Praneeth Pondicherry Ravendernath <praneeth.pondicherry@tamucc.edu>
2019-12-24 14:51:26 -0600

    Updates

---
+++ precache-manifest.bcc4d8848251d25cadbf65c8c75df087.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e71630ea9ea9cd0d7e85ebe537dbe53",
+    "url": "/face-recognition-brain/index.html"
+  },
+  {
+    "revision": "e482ff1646222886aceb",
+    "url": "/face-recognition-brain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "40b5741083229d9456db",
+    "url": "/face-recognition-brain/static/css/main.a8dbb7be.chunk.css"
+  },
+  {
+    "revision": "e482ff1646222886aceb",
+    "url": "/face-recognition-brain/static/js/2.9fe87812.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/face-recognition-brain/static/js/2.9fe87812.chunk.js.LICENSE"
+  },
+  {
+    "revision": "40b5741083229d9456db",
+    "url": "/face-recognition-brain/static/js/main.f40f1bec.chunk.js"
+  },
+  {
+    "revision": "7bd91d166675bdfd2043",
+    "url": "/face-recognition-brain/static/js/runtime-main.e9099800.js"
+  }
+]);
